42a194bb63589c82b644a1189427054ba7c62cb8
shenal chanuka <125878126+shenalchanu8@users.noreply.github.com>
2024-05-13 21:30:08 +0530

    add my firstproject

---
+++ src/data.js
@@ -0,0 +1,74 @@
+let shopItemsData = [
+    {
+        id: "A001",
+        name: "Pet Food",
+        price: 45,
+        desc: "nice the perfect food gdr jdgg cgssdy hdcgdc jdcgdngcdb.",
+        img: "image1/pet5.png",
+
+    },
+    {
+        id: "A002",
+        name: "Pet shampo",
+        price: 100,
+        desc: "nice the perfect food gdr jdgg cgssdy hdcgdc jdcgdngcdb.",
+        img: "image1/shampo.jpg",
+    },
+    {
+        id: "A003",
+        name: "Pet Collar",
+        price: 150,
+        desc: "nice the perfect food gdr jdgg cgssdy hdcgdc jdcgdngcdb.",
+        img: "image1/collar.jpg",
+    },
+   
+    {
+        id: "A004",
+        name: "Water and food bowl",
+        price: 95,
+        desc: "nice the perfect food gdr jdgg cgssdy hdcgdc jdcgdngcdb.",
+        img: "image1/plk.png",
+    },
+    {
+        id: "A005",
+        name: "Pet toys",
+        price: 46,
+        desc: "nice the perfect food gdr jdgg cgssdy hdcgdc jdcgdngcdb.",
+        img: "image1/pt.jpg",
+    },
+    {
+        id: "A006",
+        name: "Pet grooming brish",
+        price: 55,
+        desc: "nice the perfect food gdr jdgg cgssdy hdcgdc jdcgdngcdb.",
+        img: "image1/pgb.jpg",
+    },
+    {
+        id: "A007",
+        name: "Nail trimer",
+        price: 43,
+        desc: "nice the perfect food gdr jdgg cgssdy hdcgdc jdcgdngcdb.",
+        img: "image1/nt.jpg",
+    },
+    {
+        id: "A08",
+        name: "Portable pet beds",
+        price: 93,
+        desc: "nice the perfect food gdr jdgg cgssdy hdcgdc jdcgdngcdb.",
+        img: "image1/pp.jpg",
+    },
+    {
+        id: "A09",
+        name: "Toothbrush",
+        price: 73,
+        desc: "nice the perfect food gdr jdgg cgssdy hdcgdc jdcgdngcdb.",
+        img: "image1/Toothbrush.jpg",
+    },
+    {
+        id: "A010",
+        name: "Flea preventative",
+        price: 43,
+        desc: "nice the perfect food gdr jdgg cgssdy hdcgdc jdcgdngcdb.",
+        img: "image1/fp.jpg",
+    },
+];
